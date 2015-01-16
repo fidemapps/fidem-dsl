9b176fda24ac294b51f805eb10afa281b4f60ac6
Sebastien Guimont <sebastieng@sympatico.ca>
2015-01-16 09:32:03 -0500

    add literals

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
  */
 
 module.exports.challengeRules = require('./lib/challenge-rules-parser');
+module.exports.challengeRules.literals = require('./lib/challenge-rules').literals;
+
 module.exports.challengeAvailabilityRules = require('./lib/challenge-availability-rules-parser');
+module.exports.challengeAvailabilityRules.literals = require('./lib/challenge-availability-rules').literals;
+
 module.exports.reactionActions = require('./lib/reaction-actions-parser');
+module.exports.reactionActions.literals = require('./lib/reaction-actions').literals;
+
 module.exports.reactionTriggerConditions = require('./lib/reaction-trigger-conditions-parser');
+module.exports.reactionTriggerConditions.literals = require('./lib/reaction-trigger-conditions').literals;
+
 module.exports.smartListMemberConditions = require('./lib/smartlist-member-conditions-parser');
+module.exports.smartListMemberConditions.literals = require('./lib/smartlist-member-conditions').literals;
