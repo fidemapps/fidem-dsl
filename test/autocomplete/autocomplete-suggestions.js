b0c9612dfd692bcd6b10070f4282554dd92598c9
Sebastien Guimont <info@sebastienguimont.com>
2016-10-19 20:17:41 -0400

    remove promise from autocomplete tool

--- test/autocomplete/autocomplete-suggestions.js
+++ test/autocomplete/autocomplete-suggestions.js
@@ -1,219 +1,188 @@
 'use strict';
 
 var should = require('should'),
-	fs = require('fs'),
-	helper = require('../helper'),
-	PEG = require('pegjs');
+  fs = require('fs'),
+  helper = require('../helper'),
+  PEG = require('pegjs');
 var Promise = require('bluebird');
 
 var parser;
 var autocompleteOption = require('./../../lib/autocomplete/autocomplete-suggestions');
 
 describe('<Unit Test>', function () {
-	var test = function (parser, string, cursorPosition, expected) {
-		return autocompleteOption.options(parser, string, cursorPosition).then(function (result) {
-			result.should.eql(expected);
-		})
-	};
+  var test = function (parser, string, cursorPosition, expected) {
+    var result = autocompleteOption.options(parser, string, cursorPosition);
+    result.should.eql(expected);
+  };
 
-	describe('Autocomplete options', function () {
+  describe('Autocomplete options', function () {
 
-		describe('findBreak', function () {
+    describe('findBreak', function () {
 
-			it('should find [0,0] if passed the empty string', function () {
-				return autocompleteOption.findBreakPositions('', 0)
-					.then(function(positions){
-						positions.should.eql([0, 0])
-					});
-			});
+      it('should find [0,0] if passed the empty string', function () {
+        var positions = autocompleteOption.findBreakPositions('', 0);
+        positions.should.eql([0, 0])
+      });
 
-			it('should find [0,10] if only one word of length 10', function () {
-				return autocompleteOption.findBreakPositions('abcdqeushe', 10)
-					.then(function(positions) {
-						positions.should.eql([0, 10])
-					});
-			});
+      it('should find [0,10] if only one word of length 10', function () {
+        var positions = autocompleteOption.findBreakPositions('abcdqeushe', 10);
+        positions.should.eql([0, 10])
+      });
 
-			it('should find [2,10] if two word separated by a whitespace', function () {
-				return autocompleteOption.findBreakPositions('a slkjhfsd', 10)
-					.then(function(positions){
-						positions.should.eql([2, 10])
-					});
-			});
+      it('should find [2,10] if two word separated by a whitespace', function () {
+        var positions = autocompleteOption.findBreakPositions('a slkjhfsd', 10);
+        positions.should.eql([2, 10])
+      });
 
-			it('should find [5,10] if three word separated by a whitespace', function () {
-				return autocompleteOption.findBreakPositions('a ds slkjhfsd', 10)
-					.then(function(positions){
-						positions.should.eql([5, 10])
-					});
-			});
+      it('should find [5,10] if three word separated by a whitespace', function () {
+        var positions = autocompleteOption.findBreakPositions('a ds slkjhfsd', 10);
+        positions.should.eql([5, 10])
+      });
 
-			it('should find the last space before the current position', function () {
-				return autocompleteOption.findBreakPositions('a ds slkjhfsd askdjfh asdkfjh  aalskdjfh asdkfjhas dflaksjdfh asdfk', 10)
-					.then(function(positions) {
-						positions.should.eql([5, 10])
-					});
-			});
+      it('should find the last space before the current position', function () {
+        var positions = autocompleteOption.findBreakPositions('a ds slkjhfsd askdjfh asdkfjh  aalskdjfh asdkfjhas dflaksjdfh asdfk', 10);
+        positions.should.eql([5, 10])
+      });
 
-		});
+    });
 
-		describe('setBreak', function () {
+    describe('setBreak', function () {
 
-			it('should handle empty position', function () {
+      it('should handle empty position', function () {
+        var parsingString = autocompleteOption.setBreaksInString("asdlkfjhaskdjfh askdjfha lsjdfh askjfhalks ", []);
+        parsingString.should.eql([]);
+      });
 
-				return autocompleteOption.setBreaksInString("asdlkfjhaskdjfh askdjfha lsjdfh askjfhalks ", [])
-					.then(function(parsingString) {
-						parsingString.should.eql([]);
-					})
-			});
+      it('should put the "#" at the end if the position is way over the string length', function () {
+        var parsingString = autocompleteOption.setBreaksInString('this is a random string', [300]);
+        parsingString[0].should.equal('this is a random string#');
+      });
 
-			it('should put the "#" at the end if the position is way over the string length', function () {
-				return autocompleteOption.setBreaksInString('this is a random string', [300]).then(function(parsingString){
-					parsingString[0].should.equal('this is a random string#');
-				});
-			});
+      it('should put the "#" at the start if the position is negative', function () {
+        var parsingString = autocompleteOption.setBreaksInString('this is a random string', [-2]);
+        parsingString[0].should.equal('#this is a random string');
+      });
 
-			it('should put the "#" at the start if the position is negative', function () {
-				return autocompleteOption.setBreaksInString('this is a random string', [-2]).then(function(parsingString) {
-					parsingString[0].should.equal('#this is a random string');
-				});
-			});
+      it('should set the break exactly where I say so without loosing nothing of the original string', function () {
+        var parsingString = autocompleteOption.setBreaksInString('this is a random string', [3]);
+        parsingString[0].should.equal('thi#s is a random string');
+      });
 
-			it('should set the break exactly where I say so without loosing nothing of the original string', function () {
-				return autocompleteOption.setBreaksInString('this is a random string', [3]).then(function(parsingString) {
-					parsingString[0].should.equal('thi#s is a random string');
-				});
-			});
+      it('should support multiple position and produce multiple string with "#" at the corresponding position', function () {
+        var parsingStrings = autocompleteOption.setBreaksInString("member did action ea,askdjfh", [2, 5, 11]);
+        parsingStrings[0].should.equal("me#mber did action ea,askdjfh");
+        parsingStrings[1].should.equal("membe#r did action ea,askdjfh");
+        parsingStrings[2].should.equal("member did #action ea,askdjfh");
+      });
 
-			it('should support multiple position and produce multiple string with "#" at the corresponding position', function () {
+    });
 
-				return autocompleteOption.setBreaksInString("member did action ea,askdjfh", [2, 5, 11])
-					.then(function(parsingStrings) {
-						parsingStrings[0].should.equal("me#mber did action ea,askdjfh");
-						parsingStrings[1].should.equal("membe#r did action ea,askdjfh");
-						parsingStrings[2].should.equal("member did #action ea,askdjfh");
-					})
-			});
+    describe('parseStrings - challenge-availability-rules-parser', function () {
 
-		});
+      before(function (done) {
+        fs.readFile(__dirname + '/../../dsl/challenge-availability-rules-parser.pegjs', 'utf8', function (err, data) {
+          if (err) {
+            return done(err);
+          }
+          parser = PEG.buildParser(data);
+          done();
+        });
+      });
 
-		describe('parseStrings - challenge-availability-rules-parser', function () {
+      it('should return that it need to load the action code for the autocomplete action', function () {
+        var suggestion = autocompleteOption.parseStrings(parser, ["member did action #eat", "member did action eat#"], 'eat');
+        suggestion.should.eql({ codeToLoad: ['actionCode'], literal: [] });
+      });
 
-			before(function (done) {
-				fs.readFile(__dirname + '/../../dsl/challenge-availability-rules-parser.pegjs', 'utf8', function (err, data) {
-					if ( err ) {
-						return done(err);
-					}
-					parser = PEG.buildParser(data);
-					done();
-				});
-			});
+      it('should return the current word if the word is completed', function () {
+        var suggestion = autocompleteOption.parseStrings(parser, ["member did action eat #and", "member did action eat and#"], 'and');
+        suggestion.should.eql({ codeToLoad: [], literal: ['and'] });
+      });
 
-			it('should return that it need to load the action code for the autocomplete action', function () {
+      it('should only return the word being completed when the full word is entered', function () {
+        var suggestion = autocompleteOption.parseStrings(parser, ["every monday,#firday", "every monday,friday#"], 'friday');
+        suggestion.should.eql({ codeToLoad: [], literal: ['friday'] });
+      });
 
-				return autocompleteOption.parseStrings(parser, ["member did action #eat", "member did action eat#"], 'eat').then(function(suggestion){
-					suggestion.should.eql({codeToLoad: ['actionCode'], literal: []});
-				})
-			});
+      it('should return the the word being completed if not finish', function () {
+        var suggestion = autocompleteOption.parseStrings(parser, ["every #monday,#firday", "every mond#"], 'monday');
+        suggestion.should.eql({ codeToLoad: [], literal: ['monday'] });
+      });
 
-			it('should return the current word if the word is completed', function () {
+      it('will return the composed word even if half way thought', function () {
+        var suggestion = autocompleteOption.parseStrings(parser, ["member did something at #l", "member did something at l#"], 'l');
+        suggestion.should.eql({ codeToLoad: [], literal: ['at least', 'exactly', 'less than'] });
+      });
 
-				return autocompleteOption.parseStrings(parser, ["member did action eat #and", "member did action eat and#"], 'and').then(function(suggestion) {
-					suggestion.should.eql({codeToLoad: [], literal: ['and']});
-				})
-			});
+      it('will return the composed word even if almost at the end', function () {
+        var suggestion = autocompleteOption.parseStrings(parser, ["member did something at #lea", "member did something at lea#"], 'lea');
+        suggestion.should.eql({ codeToLoad: [], literal: ['at least'] });
+      });
 
-			it('should only return the word being completed when the full word is entered', function () {
+    });
 
-				return autocompleteOption.parseStrings(parser, ["every monday,#firday", "every monday,friday#"], 'friday').then(function(suggestion) {
-					suggestion.should.eql({codeToLoad: [], literal: ['friday']});
-				})
-			});
+    describe('options', function () {
 
-			it('should return the the word being completed if not finish', function () {
+      before(function (done) {
+        fs.readFile(__dirname + '/../../dsl/challenge-availability-rules-parser.pegjs', 'utf8', function (err, data) {
+          if (err) {
+            return done(err);
+          }
+          parser = PEG.buildParser(data);
+          done();
+        });
+      });
 
-				return autocompleteOption.parseStrings(parser, ["every #monday,#firday", "every mond#"], 'monday').then(function(suggestion) {
-					suggestion.should.eql({codeToLoad: [], literal: ['monday']});
-				})
-			});
+      it('should and have only an action code if ', function () {
+        return test(parser, 'member did action e', 'member did action e'.length, {
+          codeToLoad: ['actionCode'],
+          literal: []
+        });
+      });
 
-			it('will return the composed word even if half way thought', function () {
+      it('should return the current word if in a literal', function () {
+        return test(parser, "member did action eat and", "member did action eat and".length, {
+          codeToLoad: [],
+          literal: ['and']
+        });
+      });
 
-				return autocompleteOption.parseStrings(parser, ["member did something at #l", "member did something at l#"], 'l').then(function(suggestion) {
-					suggestion.should.eql({codeToLoad: [], literal: ['at least', 'exactly', 'less than']});
-				})
-			});
+      it('should return the next word if separated by a space', function () {
+        return test(parser, "member did action eat and ", "member did action eat and ".length, {
+          codeToLoad: [],
+          literal: [
+            'belongs to smartlist',
+            'challenge',
+            'every',
+            'in geofence',
+            'level',
+            'member',
+            'on',
+            'tag'
+          ]
+        });
+      });
 
-			it('will return the composed word even if almost at the end', function () {
+      it('will return all the possible word if there is a literal', function () {
+        return test(parser, 'member did action eat at ', 'member did action eat at '.length, {
+          codeToLoad: [],
+          literal: [
+            'after',
+            'and',
+            'at least',
+            'before',
+            'between',
+            'exactly',
+            'in',
+            'less than',
+            'with'
+          ]
+        });
+      })
 
-				return autocompleteOption.parseStrings(parser, ["member did something at #lea", "member did something at lea#"], 'lea').then(function(suggestion){
-					suggestion.should.eql({codeToLoad: [], literal: ['at least']});
-				})
-			});
+    });
 
-		});
+  });
 
-		describe('options', function () {
-
-			before(function (done) {
-				fs.readFile(__dirname + '/../../dsl/challenge-availability-rules-parser.pegjs', 'utf8', function (err, data) {
-					if ( err ) {
-						return done(err);
-					}
-					parser = PEG.buildParser(data);
-					done();
-				});
-			});
-
-			it('should and have only an action code if ', function () {
-				return test(parser, 'member did action e', 'member did action e'.length, {
-					codeToLoad: ['actionCode'],
-					literal: []
-				});
-			});
-
-			it('should return the current word if in a literal', function () {
-				return test(parser, "member did action eat and", "member did action eat and".length, {
-					codeToLoad: [],
-					literal: ['and']
-				});
-			});
-
-			it('should return the next word if separated by a space', function () {
-				return test(parser, "member did action eat and ", "member did action eat and ".length, {
-					codeToLoad: [],
-					literal: [
-						'belongs to smartlist',
-						'challenge',
-						'every',
-						'in geofence',
-						'level',
-						'member',
-						'on',
-						'tag'
-					]
-				});
-			});
-
-			it('will return all the possible word if there is a literal', function () {
-				return test(parser, 'member did action eat at ', 'member did action eat at '.length, {
-					codeToLoad: [],
-					literal: [
-						'after',
-						'and',
-						'at least',
-						'before',
-						'between',
-						'exactly',
-						'in',
-						'less than',
-						'with'
-					]
-				});
-			})
-
-		});
-
-	});
-
-});
+})
+;
